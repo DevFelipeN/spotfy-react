219425fd7465add8fb8fbaaf5e1acaff466cb2a6
dev_felipe <felipemartinsdonascimento4@gmail.com>
2025-02-01 17:53:45 -0400

    Componente Main adicionado ao react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Header from './Header/Header';
-import Sidebar from './Sidebar/Sidebar';
+import Sidebar from './Sideba/Sidbar';
+import Main from './Main/Main';
 
 function App() {
   return (
     <div>
       <Sidebar/>
       <Header/>
+      <Main/>
     </div>
     
   );
