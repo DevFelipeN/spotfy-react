71132697e15db1e4c6b51cf8e15004a2990d04ab
dev_felipe <felipemartinsdonascimento4@gmail.com>
2025-02-01 17:34:40 -0400

    Sidebar adicionado ao react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
 import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Sidebar/>
+      <Header/>
+    </div>
+    
   );
 }
 
